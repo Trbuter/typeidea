00b4c2e5703a71a6ec43ea57c65fbb0625ab00c1
trbuter <trbuter@qq.com>
2019-08-13 18:18:16 +0800

    add models

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -12,11 +12,14 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = '分类'
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -28,11 +31,14 @@ class Tag(models.Model):
     )
     name = models.CharField(max_length=10, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -48,9 +54,9 @@ class Post(models.Model):
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为MarkDown格式")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
-    category = models.ForeignKey(Category, verbose_name="分类")
+    category = models.ForeignKey(Category, verbose_name="分类", on_delete=models.DO_NOTHING)
     tag = models.ManyToManyField(Tag, verbose_name="标签")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -11,13 +11,14 @@ class Link(models.Model):
     )
     title = models.CharField(max_length=50, verbose_name="标题")
     href = models.URLField(verbose_name="链接")
-    status = models.PositiveIntegerField(default=1, choices=zip(range(1, 6), range(1, 6)), verbose_name="权重",
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    weight = models.PositiveIntegerField(default=1, choices=zip(range(1, 6), range(1, 6)), verbose_name="权重",
                                          help_text="权重高展示顺序靠前")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
-        verbose_name = verbose_name_plural = "友联"
+        verbose_name = verbose_name_plural = "友链"
 
 
 class SideBar(models.Model):
@@ -37,7 +38,7 @@ class SideBar(models.Model):
     display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE, verbose_name="展示类型")
     content = models.CharField(max_length=500, blank=True, verbose_name="内容", help_text="如果设置的不是HTML类型，可为空")
     status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name="状态")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
